59f1bd11b79635b58c0b5418e2520fbe418ddfc3
David Ryan <djrdev@icloud.com>
2018-02-12 14:15:34 -0700

    append works now count

--- public/scripts.js
+++ public/scripts.js
@@ -4,28 +4,30 @@ $(document).ready(function() {
 
 const appendItems = (items) => {
   items.forEach((item, index) => {
-    // make conditional -- if project.id exists don't append
     $('#card-container').prepend(`
     <article id="card${item.id}">
         <div class="title-container">
           <h4 class="title">${item.lingers}</h4>
+          <h5 class="clean">${item.clean}</h5>
         </div>
-        <h5 class="clean">${item.clean}</h5>
         <hr>
         <p class="reason" >${item.reason}</p>
       </article>`)
   })
 
-  $('.sub_menu').on('click', (event) => selectProject(event));
+  $('#card-container').on('click', (event) => selectItem(event));
 }
 
 const fetchItems = async () => {
   const itemsFetch = await fetch('http://localhost:3000/api/v1/garage/')
-   const itemsData = await itemsFetch.json()
+  const itemsData = await itemsFetch.json()
 
-  return appendItems(itemsData.items);
+  appendItems(itemsData.items)
+  cardCount(itemsData.count)
+  
+  return ;
 }
 
 const submitItem = () => {
-  
+
 }
