b2fa46b0aee83c605a8b08ae42813d9b29d12833
David Ryan <djrdev@icloud.com>
2018-02-12 10:50:39 -0700

    setup

---
+++ knexfile.js
@@ -0,0 +1,33 @@
+module.exports = {
+
+  development: {
+    client: 'pg',
+    connection: 'postgres://localhost/garage',
+    migrations: {
+      directory: `${__dirname}/db/migrations`
+    },
+    seeds: {
+      directory: `${__dirname}/db/seeds/dev`
+    },
+    useNullAsDefault: true
+  },
+  test: {
+    client: 'pg',
+    connection: 'postgres://localhost/garage_test',
+    useNullAsDefault: true,
+    migrations: {
+      directory: `${__dirname}/db/migrations`
+    },
+    seeds: {
+      directory: `${__dirname}/db/seeds/test`
+    }
+  },
+  production: {
+    client: 'pg',
+    connection: `${process.env.DATABASE_URL}?ssl=true`,
+    migrations: {
+      directory: `${__dirname}/db/migrations`
+    },
+    useNullAsDefault: true
+  }
+};
